b71d0e905b2de97cdafdfe81ef2a996b5a15f008
SongCreak <song_creak_0d@icloud.com>
2023-08-21 16:26:38 +0200

    ErrorFix

--- pages/Track.js
+++ pages/Track.js
@@ -5,7 +5,6 @@ function Track(props) {
 
   const {addOrRemoveFromPlaylist} = props;
   const {track} = props;
-  const previewUrl = track.preview_url;
 
   const handleAddClick = () => {
     addOrRemoveFromPlaylist(track.id);
@@ -22,7 +21,7 @@ function Track(props) {
     <div>
       <h4 className={styles.h4} aria-label="Track Name">{track.name.split(' (')[0]}</h4>
       <p className={styles.p} aria-label="Album Name">{track.album.name.split(' (')[0]}</p>
-      <Miniplayer previewUrl={previewUrl} onPlay={props.handlePlay}/>
+      <Miniplayer previewUrl={track.previewUrl} onPlay={props.handlePlay}/>
     </div>
       <button onClick={handleAddClick}>+|-</button>
   </li>
